0260e949d8be263dbc9edd53e979d10d481715b4
foldestibor <foldestiborlajos@gmail.com>
2019-04-24 11:47:34 +0200

    clone excercise has been finished

---
+++ week-04/day2/Clone/src/main.java
@@ -0,0 +1,14 @@
+public class main {
+  public static void main(String[] args) {
+
+
+    Student john = new Student("John", 20, "male", "BME");
+
+    Student johnTheClone = john.clone();
+
+    System.out.println(johnTheClone.getName());
+
+
+  }
+}
+
