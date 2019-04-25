d75b09332f4c14a49a90793bd9d5ea93b7e57687
foldestibor <foldestiborlajos@gmail.com>
2019-04-25 07:53:45 +0200

    Reservation: the formatin problem solved with strBuilder a reservation genarator recursion added.

---
+++ week-04/day2/Reservation/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    Reservation res = new Reservation();
+    Reservation res1 = new Reservation();
+    res.generateReservation(20);
+  }
+}
--- week-04/day2/Reservation/src/Reservation.java
+++ week-04/day2/Reservation/src/Reservation.java
@@ -1,21 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reservation implements Reservatory {
   protected String reservation;
   protected final String codeBits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  protected final String[] dowArray = {"MON", "TUE", "WED", "THU", "FRI", "SAT"};
+  protected final String[] dowArray = {"MON", "TUE", "WED", "THU", "FRI", "SAT","SUN"};
 
-
-  public String getDowBooking() {
-    dowArray
-    }
-
+  public Reservation(){
+    this.setReservation();
   }
+
+  public void setReservation(){
+    System.out.println(("#Booking " + getCodeBooking() + " for " + getDowBooking()));
+  }
+  public String getDowBooking() {
+    int num = (int)(Math.random() * 7);
+    return dowArray[num];
+    }
 
   @Override
   public String getCodeBooking() {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < 8; i++){
+      int rand = (int) (Math.random() * 36);
+      stringBuilder.append(codeBits.charAt(rand));
+    }
+    String result = stringBuilder.toString();
+    return result;
   }
-
-  public void setCodeBooking() {
-
-
+  public void generateReservation(int numOfReservations){
+    if (numOfReservations <= 0){
+      System.out.println( "That's it.");
+    }else{
+      Reservation res = new Reservation();
+      generateReservation(numOfReservations -= 1);
+  }
   }
 }
