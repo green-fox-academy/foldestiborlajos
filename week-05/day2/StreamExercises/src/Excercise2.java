97dfa18d401c981e26c6ac3af4354c5125900f1f
foldestibor <foldestiborlajos@gmail.com>
2019-05-14 12:09:17 +0200

    Excercise 2-4 is ready

---
+++ week-05/day2/StreamExercises/src/Excercise2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Excercise2 {
+
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    List resultList = numbers.stream()
+        .filter(n -> n > 0)
+        .map(n -> n * n)
+        .collect(Collectors.toList());
+
+    System.out.println(resultList);
+  }
+
+}
--- week-05/day2/StreamExercises/src/Excercise4.java
+++ week-05/day2/StreamExercises/src/Excercise4.java
@@ -1,17 +1,14 @@
-package Excercise1;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Excercise1 {
+public class Excercise4 {
 
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
     System.out.println(numbers.stream()
-        .filter(n -> n % 2 == 0)
-        .collect(Collectors.toList()));
-
+        .filter(n -> n % 2 != 0)
+        .mapToInt(n -> n)
+        .average());
   }
 
 }
